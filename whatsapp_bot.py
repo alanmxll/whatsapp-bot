535ca7cccfeef38b7921d1651760bb6d2e78afa2
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-23 17:30:15 -0300

    sendMessages function created

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -4,8 +4,28 @@ import time
 
 class WhatsappBot:
     def __init__(self):
-        self.message = 'Annihilation is always the answer. We destroy parts of ourselves every day. We Photoshop our warts away. We edit the parts we hate about ourselves, modify the parts we think people hate. We curate our identity, carve it, distill it. Annihilation is all we are.'
-        self.groups = ['Coronapru 💥', 'SB // A New Hope']
+        self.message = "Annihilation is always the answer. We destroy parts of ourselves every day. We Photoshop our warts away. We edit the parts we hate about ourselves, modify the parts we think people hate. We curate our identity, carve it, distill it. Annihilation is all we are."
+        self.groups = ["Coronapru 💥", "SB // A New Hope "]
         options = webdriver.ChromeOptions()
-        options.add_argument('lang=eng')
-        self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
+        options.add_argument("lang=eng")
+        self.driver = webdriver.Chrome(executable_path=r"./chromedriver")
+
+    def sendMessages(self):
+        self.driver.get("https://web.whatsapp.com/")
+        time.sleep(30)
+
+        for group in self.groups:
+            group = self.driver.find_element_by_xpath(
+                f"//span[@title='{group}']")
+            time.sleep(3)
+            group.click()
+            chat_box = self.driver.find_element_by_class_name(
+                "_3ko75 _5h6Y_ _3Whw5")
+            time.sleep(3)
+            chat_box.click()
+            chat_box.send_keys(self.message)
+            send_button = self.driver.find_element_by_xpath(
+                "//span[@data-icon='send']")
+            time.sleep(3)
+            send_button.click()
+            time.sleep(5)
