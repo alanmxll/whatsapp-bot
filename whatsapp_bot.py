2e487540a412dff8ae7b6ad8e3d0d49f0c34b6d4
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-24 14:35:50 -0300

    Div xpath string where the bot types passed into a variable to avoid code repetition

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -8,19 +8,18 @@ class WhatsappBot:
     def __init__(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get("https://web.whatsapp.com/")
+        self.xpath = "//div[contains(@class,'copyable-text selectable-text')]"
         time.sleep(15)
 
     def __find_contact(self, contact):
-        search_field = self.driver.find_element_by_xpath(
-            "//div[contains(@class,'copyable-text selectable-text')]")
+        search_field = self.driver.find_element_by_xpath(self.xpath)
         time.sleep(3)
         search_field.click()
         search_field.send_keys(contact)
         search_field.send_keys(Keys.ENTER)
 
     def __send_message(self, message):
-        message_field = self.driver.find_elements_by_xpath(
-            "//div[contains(@class,'copyable-text selectable-text')]")
+        message_field = self.driver.find_elements_by_xpath(self.xpath)
         message_field[1].click()
         time.sleep(3)
         message_field[1].send_keys(message)
