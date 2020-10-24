b098f2554b46951f282d0c5e16027b5a967e3492
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-24 20:04:07 -0300

    docstrings

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -6,12 +6,21 @@ import time
 
 class WhatsappBot:
     def __init__(self):
+        """
+        -> Start the necessary variables to the next methods.
+        @param self.
+        """
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get("https://web.whatsapp.com/")
         self.xpath = "//div[contains(@class,'copyable-text selectable-text')]"
         time.sleep(15)
 
     def __find_contact(self, contact):
+        """
+        -> Finds a contact whose name was passed as a parameter.
+        @param self.
+        @param contact: str
+        """
         search_field = self.driver.find_element_by_xpath(self.xpath)
         time.sleep(3)
         search_field.click()
@@ -19,6 +28,11 @@ class WhatsappBot:
         search_field.send_keys(Keys.ENTER)
 
     def __send_message(self, message):
+        """
+        -> Send the message passed by parameter to the found contact.
+        @param self.
+        @param message: str
+        """
         message_field = self.driver.find_elements_by_xpath(self.xpath)
         message_field[1].click()
         time.sleep(3)
@@ -26,6 +40,12 @@ class WhatsappBot:
         message_field[1].send_keys(Keys.ENTER)
 
     def execute(self, contacts, message):
+        """
+        -> Run the __find_contact & __send_message private methods
+        @param self.
+        @param contacts: list<str>
+        @param message: str
+        """
         for contact in contacts:
             self.__find_contact(contact)
             self.__send_message(message)
